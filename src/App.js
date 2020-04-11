e30d574fff8ccb55998fa7416e5a14812c1615a9
tobechukwuobitube <tobechukwuobitube@gmail.com>
2020-04-11 20:34:04 +0100

    build out estimator frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import Header from './components/Header'
+import React from "react";
+import Header from "./components/Header/Header";
+import EstimatorForm from "./components/EstimatorForm";
 
 function App() {
   return (
-    <div className="App">
-<Header />
+    <div style={{ background: "white", color: "black" }}>
+      <Header />
+      <EstimatorForm />
     </div>
   );
 }
